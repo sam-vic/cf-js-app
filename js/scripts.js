4dac23d0db293c6983cf62ba20d73c5415a79c47
SamLCH <64434536+sam-vic@users.noreply.github.com>
2023-01-30 13:44:02 -0800

    set font family, looped product line cost with condition based off type

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,34 @@ let listOfGreens = [
     { name: 'Mini kale', cost: 15, types: 'micro-greens' },
     { name: 'Basil', cost: 5, types: 'herbs' }
 ]
+
+
+for ( i=0; i<listOfGreens.length; i++ ) {
+    //variable for iterator before it is declared
+    let x = i
+    // framework of listing the product
+    let textStructure = (
+        'Product: ' + listOfGreens[x].name + '; ' +
+        'Cost: ' + listOfGreens[x].cost + '; ' +
+        'Type: ' + listOfGreens[x].types + '; ' + '<br/>'
+    )
+
+    //loop to display product line
+    if (listOfGreens[i].types === 'greens' ) {
+        document.write(
+            textStructure + 'Sold per head' + '<br/>'
+        )
+
+    } else if (listOfGreens[i].types === 'micro-greens') {
+
+        document.write(
+            textStructure + 'Sold per 100g' + '<br/>'
+        )
+
+    } else {
+        document.write(
+            textStructure + 'Sold per shell' + '<br/>'
+        )
+    }
+}
+
