040def13851d01aad20a22bd95dea2e1150493d9
SamLCH <64434536+sam-vic@users.noreply.github.com>
2023-02-05 13:43:14 -0800

    condense loop function and added basic layout for displayed data

--- js/scripts.js
+++ js/scripts.js
@@ -14,52 +14,60 @@ let listOfGreens = (function () {
                 data.push(newProduct) : 'Wrong data type'
         },
 
-        // call for new data array
+        // call for newest data array
         getAll: function () {
             return (
                 data
             )
+        },
+        // framework to append data loop into elements 
+        addListItem: function (items) {
+            //element selector to append product-list to ul
+            let productSelector = document.querySelector('.product-list')
+
+            let listItem = (
+                document.createElement('li')
+            )
+
+            let button = (
+                document.createElement('button')
+            )
+
+            // framework of listing the product
+            let textStructure = (
+                'Product: ' + items.name + '; ' +
+                'Cost: ' + items.cost + '; ' +
+                'Type: ' + items.types + '; '
+            )
+
+            //conditional testing for how product sold
+            if (items.types === 'greens') {
+                button.innerText = textStructure + 'Sold per head'
+            } else if (items.types === 'micro-greens') {
+                button.innerText = textStructure + 'Sold per 100g'
+            } else if (items.types === 'herbs') {
+                button.innerText = textStructure + 'Sold per shell'
+            } else {
+                //In-case product type is not available
+                return button.innerText = 'Product undefined'
+            }
+
+            button.classList.add('button')
+            listItem.appendChild(button)
+            productSelector.appendChild(listItem)
         }
     }
 })();
-
+// addition of new products
 listOfGreens.add({ name: 'Thymn', cost: 4, types: 'herbs' })
+
+
 
 //loop function to display product line
 listOfGreens.getAll().forEach(items => {
-    // framework of listing the product
-    let textStructure = (
-        '<div>' + 
-            'Product: ' + items.name + '; ' +
-            'Cost: ' + items.cost + '; ' +
-            'Type: ' + items.types + '; ' +
-        '</div>'
-    )
-
-    //conditional testing for how product sold
-    if (items.types === 'greens') {
-        document.write(
-            textStructure + 'Sold per head' + '<br/>'
-        )
-
-    } else if (items.types === 'micro-greens') {
-
-        document.write(
-            textStructure + 'Sold per 100g' + '<br/>'
-        )
-
-    } else if (items.types === 'herbs') {
-        document.write(
-            textStructure + 'Sold per shell' + '<br/>'
-        )
-    } else {
-        //In-case product type is not available
-        return document.write('Product undefined')
-    }
+    listOfGreens.addListItem(items)
 })
 
 // Filter function for product specificity
-
-
 const productFilter = listOfGreens.getAll().filter(item => item.types === 'greens')
 console.log(productFilter, '123213')
