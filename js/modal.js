5c373cc46a2d70d30a4888dbfeb5c2364e4a5a98
SamLCH <64434536+sam-vic@users.noreply.github.com>
2023-02-13 11:33:01 -0800

    moved the element code from index file to js file, and added new functionality to closing modal

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,39 @@
 let modalFunction = (function () {
-    function showModal() {
-        let modalContainer = document.querySelector('#modal-container')
-        modalContainer.classList.add('is-visible')
+    function showModal(title, text) {
+        let modalContainer = document.querySelector('#modal-container');
+
+        // Clear all existing modal content
+        modalContainer.innerHTML = '';
+
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+
+        // Add the new modal content
+        let closeButtonElement = document.createElement('button');
+        closeButtonElement.classList.add('modal-close');
+        closeButtonElement.innerText = 'Close';
+        closeButtonElement.addEventListener('click', event => {
+            hideModal()
+        })
+
+        let titleElement = document.createElement('h1');
+        titleElement.innerText = title;
+
+        let contentElement = document.createElement('p');
+        contentElement.innerText = text;
+
+        modal.appendChild(closeButtonElement);
+        modal.appendChild(titleElement);
+        modal.appendChild(contentElement);
+        modalContainer.appendChild(modal);
+
+        modalContainer.classList.add('is-visible');
+        modalContainer.addEventListener('click', (event) => {
+            let target = event.target;
+            if (target === modalContainer) {
+              hideModal();
+            }
+        });
     }
 
     function hideModal() {
@@ -10,9 +42,13 @@ let modalFunction = (function () {
     }
 
     document.querySelector('#show-modal').addEventListener('click', event => {
-        showModal()
+        showModal('Modal Title', 'This is the modal content!')
     })
-    document.querySelector('.modal-close').addEventListener('click', event => {
-        hideModal()
+
+    window.addEventListener('keydown', event => {
+        let modalContainer = document.querySelector('#modal-container');
+        if (event.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+          hideModal();  
+        }
     })
 })()
