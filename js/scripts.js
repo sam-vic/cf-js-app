4e070d58f38623e6cccc46a99213ab34e9ee3a9a
SamLCH <64434536+sam-vic@users.noreply.github.com>
2023-02-28 11:52:37 -0800

    Feat!: got the search and modal to hook up with modal showing data details

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ import { showModal } from './modal.js'
 
 let listOfData = (function () {
     let data = []
-    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=4';
 
     return {
         // addition of new data to end of array
@@ -120,3 +120,10 @@ listOfData.loadList().then(function () {
         listOfData.addListItem(items)
     })
 })
+
+export function showDetails(item) {
+    listOfData.loadDetails(item).then(function () {
+        console.log(item, 'showDetails', item.name)
+        showModal(item.name, item.height, item.imageUrl)
+    })
+}
